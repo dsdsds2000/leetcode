723bdcb5e5b4b39a706c00568efb83ca0284b564
dsdsds2000 <414755884@qq.com>
2016-06-10 01:22:58 +0800

    344. Reverse String 476 / 476 test cases passed. Status: Accepted Runtime: 12 ms Your runtime beats 29.50% of cppsubmissions.

--- leetcode/leetcode.cpp
+++ leetcode/leetcode.cpp
@@ -7,7 +7,14 @@ class Solution
 public:
 	string reverseString(string s)
 	{
-		reverse(s.begin(), s.end());
+		char t;
+		int n = s.size();
+		for (int i = 0; i < n / 2; i++)
+		{
+			t = s[i];
+			s[i] = s[n - 1 - i];
+			s[n - 1 - i] = t;
+		}
 		return s;
 	}
 };
